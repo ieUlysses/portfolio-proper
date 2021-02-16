c613d21c3d675296a0d93392a48debba73a8cf49
James Conway <ire.ulysses@gmail.com>
2021-02-16 09:11:52 +0100

    first push

---
+++ script.js
@@ -0,0 +1,33 @@
+let mqlM = window.matchMedia('(max-width: 767px)');
+
+let mqlF = window.matchMedia('(min-width: 1024px)');
+// function loader() {
+if (mqlM.matches) {
+    let html =
+        `
+        <div class="intro" id="div-1">
+            <h3 id="intro-txt">
+                I'm James, and welcome to my World!
+                A budding Fullstack developer with a pasion for
+                <strong>Ux</strong> and <strong>Ui Design</strong>
+            </h3>
+
+            <img id="headshot" src="./images/New folder/4.svg"
+                alt="a photo of James Conway, the creater of this odd little world">
+
+        </div>
+`
+    document.getElementById("welcome").innerHTML = html;
+
+} else if (mqlF.matches) {
+    html =
+        `
+        <h1>Hello cutie<h/1>
+        `
+
+    document.getElementById("welcome").innerHTML = html
+}
+// }
+// loader()
+
+// document.getElementById("headshot").addEventListener("load", loader())
